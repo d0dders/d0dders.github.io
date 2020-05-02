ab888f130878fbefbb02a16af994d9ca881fd155
d0dders <dodders@outlook.com>
2020-05-02 18:35:39 +0100

    Added some magic sparkle and fixed a layout issue caused by contact buttons

--- js/site.js
+++ js/site.js
@@ -2,6 +2,22 @@
 $('body').scrollspy({ target: '#navbar' , offset: 100});
 
 //AWWW SNAP!!
-$(".content").click(function(){
-    $(".content").fadeOut();
-  });
+$(".avatar").click(function(){
+    $(".avatar").fadeOut();
+});
+
+
+// MAGIC SPARKLES
+$('#hp-game').sparkle({
+  color: "rainbow",
+  count: 50,
+  overlap: 0,
+  speed: 2,
+  minSize: 7,
+  maxSize: 10,
+  direction: "both"
+}).trigger("start.sparkle").off("mouseover.sparkle")
+.off("mouseout.sparkle")
+.off("focus.sparkle")
+.off("blur.sparkle");
+
